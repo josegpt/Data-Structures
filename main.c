3f896793016b01276715b725b065b6d8126293c9
Jose G Perez Taveras <josegpt27@gmail.com>
2021-02-13 15:00:46 -0500

    clean main

--- main.c
+++ main.c
@@ -1,31 +1,9 @@
 #include <stdio.h>
-#include "lib/utils/utils.h"
-#include "lib/stack/stack.h"
-#include "lib/sorting/sorting.h"
-#include "lib/heaps/max_heap.h"
 
 int main(void)
 {
   /* int arr[] = {5, 9, 2, 1, 3, 6, 0, 4, 7, 8}; */
   /* int arr_size = sizeof(arr) / sizeof(arr[0]); */
-  // bubble_sort(arr, arr_size);
-  // selection_sort(arr, arr_size);
-  // insertion_sort(arr, arr_size);
-
-  // Heap* h = heap_create();
-  // for (int i = 0; i < arr_size; ++i) {
-  //   heap_insert(h, i);
-  // }
-
-  // int* a = heap_sort(h);
-
-  // print_list(a, arr_size, ' ');
-
-  /* Stack* stack = stack_create(); */
-  /* stack_push(stack, 2); */
-  /* stack_push(stack, 3); */
-  /* stack_pop(stack); */
-  /* printf("%d\n", stack_top(stack)); */
 
   return 0;
 }
